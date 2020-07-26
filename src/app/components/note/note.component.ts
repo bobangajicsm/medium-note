1de0ee96f4f7045c48016b3855c06c4013dc3595
Slobodan Gajic <freelancerslobodan@gmail.com>
2020-07-26 14:34:18 +0200

    added MathJax

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -3,7 +3,9 @@ import MediumEditor from "medium-editor";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { AuthService } from "src/app/auth.service";
 import { AngularFireAuth } from "@angular/fire/auth";
+import { analytics } from 'firebase';
 
+var MathJax: any;
 @Component({
   selector: "app-note",
   templateUrl: "./note.component.html",
@@ -11,8 +13,10 @@ import { AngularFireAuth } from "@angular/fire/auth";
 })
 export class NoteComponent implements OnInit {
   @ViewChild("medium", { static: true }) medium: ElementRef;
+  @ViewChild("output", { static: true }) output: ElementRef;
   user;
   editor;
+  note;
 
   constructor(
     private firestore: AngularFirestore,
@@ -25,6 +29,15 @@ export class NoteComponent implements OnInit {
     this.editor = new MediumEditor(edit);
 
     this.editor.subscribe("editableInput", (event, element: HTMLElement) => {
+      this.note = this.editor.getContent();
+      var mySubString = this.note.substring(
+        this.note.indexOf("$") + 1, 
+        this.note.lastIndexOf("$")
+    );
+
+    if(mySubString.length > 1) {
+      this.convert(mySubString);
+    }
       this.updateNote(this.editor.getContent());
     });
   }
@@ -46,6 +59,39 @@ export class NoteComponent implements OnInit {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  convert(input) {
+    let out = this.output.nativeElement;
+    out.innerHTML = '';
+    //
+    //  Reset the tex labels (and automatic equation numbers, though there aren't any here).
+    //  Get the conversion options (metrics and display settings)
+    //  Convert the input to CommonHTML output and use a promise to wait for it to be ready
+    //    (in case an extension needs to be loaded dynamically).
+    //
+    window['MathJax'].texReset();
+    var options = window['MathJax'].getMetricsFor(out);
+    window['MathJax'].tex2chtmlPromise(input, options).then(function (node) {
+      //
+      //  The promise returns the typeset node, which we add to the output
+      //  Then update the document to include the adjusted CSS for the
+      //    content of the new equation.
+      //
+      out.appendChild(node);
+      window['MathJax'].startup.document.clear();
+      window['MathJax'].startup.document.updateDocument();
+    }).catch(function (err) {
+      //
+      //  If there was an error, put the message into the output instead
+      //
+      console.log(err);
+      out.appendChild(document.createElement('pre')).appendChild(document.createTextNode(err.message));
+    }).then(function (e) {
+      //
+      //  Error or not, re-enable the display and render buttons
+      //
+    });
   }
 
   ngOnInit() {
